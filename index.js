0ff2493e6e62dd9c6e76c509fd7ebce79ad49667
Andrew Marshall and Julich Mera <pair+andrew+julich@casecommons.org>
2016-12-05 10:32:49 -0500

    Return RAF requestId so cancelAnimationFrame can be used

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = debounce
 function debounce(fn, now) {
   var args = null
   var ctx = null
+  var requestId = null
 
   // I don't know why, but adding this
   // comment seems to stop uglify from
@@ -14,16 +15,18 @@ function debounce(fn, now) {
   return debounced
 
   function debounced() {
-    if (args !== null) return
+    if (requestId !== null) return requestId
     args = slice(arguments)
     ctx = this
     if (now) fn.apply(ctx, args)
-    raf(next)
+    requestId = raf(next)
+    return requestId
   }
 
   function next() {
     if (!now) fn.apply(ctx, args)
     args = null
     ctx = null
+    requestId = null
   }
 }
